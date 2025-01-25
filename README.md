03d2ba3f93856a5b11f36417210d2fa39fa582c1
Jagger Chia <jiaquanchao@gmail.com>
2025-01-25 22:22:08 +0800

    tmp

---
+++ README.md
@@ -0,0 +1,65 @@
+# Portfolio Design
+
+> This project is a Responsive version of my portfolio design, the website looks good (and readable) on all screens.
+
+
+## Built With
+
+- HTML & CSS
+
+## Live Demo
+
+🔥 [Click here](https://abdulhamiid.github.io/portfolio/) 🔥
+
+## Screenshots
+![screenshot](./img/portfolio1.PNG)
+![screenshot](./img/portfolioPopup.PNG)
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Clone this repository
+
+```
+git clone https://github.com/abdulhamiid/portfolio.git
+```
+
+Open project derectory
+```
+cd portfolio
+```
+
+## Getting Started
+
+### Deployment
+
+open the ```index.html``` on your Browser
+
+
+## Author
+
+👤 **Abdulhamid**
+
+- GitHub: [abdulhamiid](https://github.com/abdulhamiid)
+- Twitter: [@abdulhamid_adio](https://twitter.com/abdulhamid_adio)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/abdulhamid-adio)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/abdulhamiid/portfolio/issues).
+
+## Show your support
+
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Thanks to microverse
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ javascript/index.js
@@ -0,0 +1,182 @@
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('header h3');
+const hamburger = document.querySelector('#openmenu');
+const closeHamburger = document.querySelector('#closemenu');
+const ul = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+const menuBg = document.querySelector('.menu-bg');
+const lis = Array.from(navLinks);
+const btn = document.querySelectorAll('.action-btn');
+
+const navbar = document.querySelector('.mynav');
+window.onscroll = () => {
+  if (window.scrollY > 40) {
+    navbar.classList.add('nav-active');
+  } else {
+    navbar.classList.remove('nav-active');
+  }
+};
+
+function toggler() {
+  logo.classList.toggle('hide');
+  ul.classList.toggle('hide');
+  menuBg.classList.toggle('remove');
+}
+
+menu.addEventListener('click', () => {
+  toggler();
+  hamburger.classList.toggle('remove');
+  closeHamburger.classList.toggle('remove');
+  document.body.classList.toggle('no-scroll');
+});
+
+for (let i = 0; i < lis.length; i += 1) {
+  lis[i].addEventListener('click', () => {
+    toggler();
+    closeHamburger.classList.add('remove');
+    hamburger.classList.remove('remove');
+    document.body.classList.remove('no-scroll');
+  });
+}
+
+// mobile
+const myProject = [{
+  id: 0,
+  name: 'Conference Website',
+  description: 'This is a responsive website about BDS common summit which is about International medical tourist visit in Nigeria for a whole range of dental care. The template was originally designed by Cindy Shin in Behance.',
+  technologies: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+  source: 'https://github.com/abdulhamiid/capstone-project',
+  image: 'img/bdsconference.PNG',
+  'live version': 'https://bds-conference.netlify.app',
+}, {
+  id: 1,
+  name: 'GymFit',
+  description: 'GymFit is a web application that allows users to view and reserve multiple fitness activities and a fitness activity can have multiple reservations on separate dates.',
+  technologies: ['Ruby on rails', 'Ruby', 'JavaScript', 'Divise', 'React'],
+  source: 'https://github.com/abdulhamiid/final-capstone-back-end',
+  image: 'img/GymFit.PNG',
+  'live version': 'https://final-capstone-front-lfmdhnejp-developerwaleed.vercel.app/',
+}, {
+  id: 2,
+  name: 'Covid-19 Tracker',
+  description: 'Covid Tracker is a web application to present the numerical data about corona virus pandemic, fetching the data from an API.',
+  technologies: ['React', 'Redux', 'JavaScript', 'ES6 Modules', 'Bootstrap'],
+  source: 'https://github.com/abdulhamiid/covid-19',
+  image: './img/covid.PNG',
+  'live version': 'https://vcovid-19-tracker.netlify.app',
+}, {
+  id: 3,
+  name: 'Math Magicians',
+  description: 'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations, App also display random math-related quote.',
+  technologies: ['HTML', 'CSS', 'React'],
+  source: 'https://github.com/abdulhamiid/calculator',
+  image: 'img/math-magicains.PNG',
+  'live version': 'https://maths-magician-webapp.netlify.app',
+}, {
+  id: 4,
+  name: 'Awesome Books',
+  description: 'Awesome books is a single page app (SPA) website that allows users to keep track of books in their library by adding to and removing books from a list.',
+  technologies: ['JavaScript', 'Webpack', 'CSS', 'ES6 modules'],
+  source: 'https://github.com/abdulhamiid/awesome-books-es6-modules',
+  image: 'img/awesome-list.PNG',
+  'live version': 'https://abdulhamiid.github.io/awesome-books-es6-modules',
+}, {
+  id: 5,
+  name: 'Bookstore',
+  description: 'This is an Online library application that enables user to keep track of books in their library by adding to and removing books from a list, user can also check the percentage read per book.',
+  technologies: ['React', 'Redux', 'HTML', 'CSS'],
+  source: 'https://github.com/abdulhamiid/bookstore',
+  image: './img/bookstore1.PNG',
+  'live version': 'https://grand-entremet-4178eb.netlify.app/',
+}];
+
+function createPopup(idx, prop) {
+  document.body.classList.add('no-scroll');
+  const subject = document.querySelector('#work-section');
+  subject.insertAdjacentHTML('afterend',
+    `<div id="popup-window" class="">
+  <div>
+  <div>
+  <i class="bi bi-x-circle-fill"></i>
+  <img src="${prop[idx].image}" alt="project" class="stretch project-img">
+  </div>
+  <div class="flex-column">
+  <h2>${prop[idx].name}</h2>
+  <ul class="flex">
+  ${prop[idx].technologies.map((item) => `<li class="li-padding bg-ash tech">${item}</li>`).join('')}
+  </ul>
+  <p>${prop[idx].description}</p>
+  <div class="btn-action stretch flex">
+  <a href="${prop[idx]['live version']}" target="_blank"><button type="button" class="btn-bg-green flex" id="popup-btn">See Live<img src="img/see-live-icon.png" alt="see-live-icon"></button></a>
+  <a href="${prop[idx].source}" target="_blank"><button type="button" class="btn-bg-green flex" id="popup-btn">See Source<img src="img/Vector.png" alt="github-logo"></button></a>
+  </div>
+  </div>
+  </div>
+  </div>`);
+}
+
+function dltbtn(dbtn) {
+  dbtn.addEventListener('click', () => {
+    document.body.classList.remove('no-scroll');
+    document.querySelector('#popup-window').setAttribute('style', 'left: -2000px; transition: 0.8s ease;');
+  });
+}
+
+// popup
+function handleClick(e) {
+  const index = e.target.id;
+  createPopup(index, myProject);
+  const btnClose = document.querySelector('.bi-x-circle-fill');
+  dltbtn(btnClose);
+}
+
+btn.forEach((item) => {
+  item.addEventListener('click', handleClick);
+});
+const email = document.querySelector('#mail');
+const form = document.querySelector('form');
+const errorMsg = document.querySelector('#message');
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const regex = /[A-Z]/g;
+  const userEmail = email.value;
+
+  if (userEmail.match(regex)) {
+    if (!errorMsg.classList.contains('error')) {
+      errorMsg.classList.add('error');
+      const icon = document.createElement('i');
+      icon.classList.add('fa-sharp', 'fa-solid', 'fa-circle-info');
+      errorMsg.appendChild(icon);
+      const p = document.createElement('p');
+      p.innerHTML = 'Email should be in lower case';
+      errorMsg.appendChild(p);
+
+      setTimeout(() => {
+        errorMsg.innerHTML = '';
+        errorMsg.classList.remove('error');
+      }, 3000);
+    }
+  } else {
+    errorMsg.remove();
+    form.submit();
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+
+let formxx = { name: '', mail: '', msg: '' };
+
+if (localStorage.getItem('portfolio') !== null) {
+  const newdata = localStorage.getItem('portfolio');
+  formxx = JSON.parse(newdata);
+}
+const inputs = document.querySelectorAll('input, textarea');
+inputs.forEach((item) => {
+  item.value = formxx[item.id];
+  item.addEventListener('input', (e) => {
+    formxx[e.target.id] = e.target.value;
+    const data = JSON.stringify(formxx);
+    localStorage.setItem('portfolio', data);
+  });
+});
